dffa5ab01faf8aeca140cfd8746e51e809d3608e
AliBoka <aliboka71@gmail.com>
2022-04-07 21:50:40 +0430

    feat : add navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import '../index.css'
+const Navbar = () => {
+  return (
+    <div className="flex items-center w-full m-6  font-paytoneone">
+      <div className="flex justify-center h-16 w-36  bg-no-repeat items-center bg-bg-logo text-2xl pr-4 pb-2"
+      
+      >
+          ALPINE
+      </div>
+      <div className="ml-14">
+        <ul className="flex">
+          <li className="px-4 cursor-pointer py-3">ABOUT US</li>
+          <li className="px-4 cursor-pointer py-3">PRICING</li>
+          <li className="px-4 cursor-pointer py-3">MOBILE</li>
+          <li className="px-4 cursor-pointer py-3">BLOG</li>
+        </ul>
+    
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
