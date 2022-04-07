feb9f7e124cfc0aba3ad218614a0754cade00f75
AliBoka <aliboka71@gmail.com>
2022-04-08 02:55:46 +0430

    feat : add main container style to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,17 @@ module.exports = {
     extend: {
       backgroundImage : {
         'bg-logo' : "url('/src/assets/logo-img.png')",
-        'first-bg' : "url('/src/assets/Alpine.png')"
+        'first-bg' : "url('/src/assets/Alpine.png')",
+        'linear-bg' : "linear-gradient(180deg, rgba(50, 42, 39, 0) 0%, rgba(45, 40, 40, 0.85) 33.85%, #2D2828 49.48%, #2D2828 68.75%, #2D2828 82.29%, #2D2828 100%)"
       },
+      colors : {
+        'rgba-gray' : 'rgba(255, 255, 255, 0.2)',
+        'rgba-white' : 'rgba(255, 255, 255, 0.4)'
+
+      },
+      boxShadow : {
+        'rgba-shadow' : "-15px 0px 15px rgba(0, 0, 0, 0.3)"
+      }
     
     },
   },
