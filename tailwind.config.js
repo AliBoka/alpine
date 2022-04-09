faa4eb0b8b289f3c2ebec2b04977210fc29017df
AliBoka <aliboka71@gmail.com>
2022-04-10 00:31:50 +0430

    feat : add style to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,16 +10,21 @@ module.exports = {
       backgroundImage : {
         'bg-logo' : "url('/src/assets/logo-img.png')",
         'first-bg' : "url('/src/assets/Alpine.png')",
-        'linear-bg' : "linear-gradient(180deg, rgba(50, 42, 39, 0) 0%, rgba(45, 40, 40, 0.85) 33.85%, #2D2828 49.48%, #2D2828 68.75%, #2D2828 82.29%, #2D2828 100%)"
+        'linear-bg' : "linear-gradient(180deg, rgba(50, 42, 39, 0) 0%, rgba(45, 40, 40, 0.85) 33.85%, #2D2828 49.48%, #2D2828 68.75%, #2D2828 82.29%, #2D2828 100%)",
+         'white-bg' : "url('/src/assets/Rectangle 13.png')",
       },
       colors : {
         'rgba-gray' : 'rgba(255, 255, 255, 0.2)',
-        'rgba-white' : 'rgba(255, 255, 255, 0.4)'
+        'rgba-white' : 'rgba(255, 255, 255, 0.4)',
+        'light-gray' : '#C4C4C4',
+        
 
       },
       boxShadow : {
         'rgba-shadow' : "-15px 0px 15px rgba(0, 0, 0, 0.3)"
       }
+      ,
+      backgroundColor : ['checked']
     
     },
   },
